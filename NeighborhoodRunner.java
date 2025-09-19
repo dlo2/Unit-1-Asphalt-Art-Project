7e5ac12c7c0fa22c04262ad47b84bb6e7b9a85e3
dlo2 <jdangelo0002@mymail.lausd.net>
2025-09-18 21:56:58 -0700

    finished project

---
+++ NeighborhoodRunner.java
@@ -0,0 +1,19 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+    
+  BackgroundPainter bob = new BackgroundPainter();
+    bob.paintBackground("white");
+
+
+  MinionPainter Sturat = new MinionPainter();
+    Sturat.drawMinion();
+
+  MinionPainter2 Bob = new MinionPainter2();
+    Bob.drawMinion2();
+
+    
+  }
+
+}
